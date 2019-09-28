dc38c1994e0f57992886b50a0b0e819097f8e54c
Arthur Nakano <arthurnakano@Leticias-MacBook-Air.local>
2019-09-28 15:07:46 -0300

    clone das animacoes completas

--- common/functions.js
+++ common/functions.js
@@ -8,84 +8,44 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
-(function ($) {
+(function () {
   var images = _toConsumableArray(document.getElementById("images").children);
 
-  var numImages = images.length;
-  var current = Math.floor(Math.random() * numImages);
-  var INTERVAL = 5000;
-  var init_timers = [];
+  var currentImage = "";
+  var randomImage = Math.floor(Math.random() * images.length);
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      images[randomImage].classList = " current";
+      start();
+    }, 800);
+  });
 
   function start() {
-    timer = setTimeout(function () {
+    setTimeout(function () {
+      var oldImage = currentImage !== "" ? currentImage : Math.floor(Math.random() * images.length);
       images.map(function (image) {
-        image.classList = "hide";
-        subTimer = setTimeout(function () {
-          image.classList = "";
-        }, 2200);
-        current = (_readOnlyError("current"), getRandomNext());
-        image.classList = "current";
-        start();
+        image.classList = "";
       });
-    }, INTERVAL);
+      images[oldImage].classList += " hide";
+      setTimeout(function () {
+        images[oldImage].classList = "";
+      }, 2200);
+      currentImage = randomNext(oldImage);
+      images[currentImage].classList += " current";
+      start();
+    }, 3000);
   }
 
-  function getRandomNext() {
-    var n = Math.floor(Math.random() * numImages);
+  function randomNext(oldImage) {
+    var newImage = Math.floor(Math.random() * images.length);
 
-    if (n == current) {
-      while (n == current) {
-        n = Math.floor(Math.random() * numImages);
+    if (newImage == oldImage) {
+      while (newImage == oldImage) {
+        newImage = Math.floor(Math.random() * images.length);
       }
     }
 
-    return n;
-  } // 
-  // $(window).one('enter.page.bq', function(){
-  // init_timers[init_timers.length] = setTimeout(function(){
-  // $title.addClass('show');
-  // }, 0);
-  // init_timers[init_timers.length] = setTimeout(function(){
-  // $images.eq(current).addClass('current');
-  // start();
-  // }, 800);
-  // init_timers[init_timers.length] = setTimeout(function(){
-  // $('#scroll-info').addClass('show');
-  // }, 1300);
-  // init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-  // }).one('exit.page.bq', function(){
-  // removeEvents();
-  // stop();
-  // for (var i = init_timers.length - 1; i >= 0; i--) {
-  // clearTimeout(init_timers[i]);
-  // }
-  // })
-
-})(); // var timer = subTimer = null;
-// function stop(){
-//     clearTimeout(timer);
-//     clearTimeout(subTimer);
-// }
-// var init_timers = []
-// $(window).one('enter.page.bq', function(){
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $title.addClass('show');
-//     }, 0);
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $images.eq(current).addClass('current');
-//         start();
-//     }, 800);
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $('#scroll-info').addClass('show');
-//     }, 1300);
-//     init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-// }).one('exit.page.bq', function(){
-//     removeEvents();
-//     stop();
-//     for (var i = init_timers.length - 1; i >= 0; i--) {
-//         clearTimeout(init_timers[i]);
-//     }
-// })
+    return newImage;
+  }
+})();
 //# sourceMappingURL=functions.js.map
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,97 +1,47 @@
-(function($){
+(() => {
 
     const images = [...(document.getElementById("images").children)]
-    const numImages = images.length
-    const current = Math.floor(Math.random()*numImages)
-    const INTERVAL = 5000
-    const init_timers = []
+    let currentImage = ""
+    const randomImage = Math.floor(Math.random() * images.length)
+
+    document.addEventListener("DOMContentLoaded", function() {
+		setTimeout(function() {
+            images[randomImage].classList = " current"
+			start()
+		}, 800)
+    })
+
 
     function start() {
-        timer = setTimeout(function() {
+		setTimeout(function(){
+            let oldImage = currentImage !== "" ? currentImage : Math.floor(Math.random() * images.length) 
+
             images.map((image) => {
-                image.classList = "hide"
-
-                subTimer = setTimeout(function(){
-                    image.classList = ""
-                },2200)
-
-                current = getRandomNext()
-                image.classList = "current"
-                start()
+                image.classList = ""
             })
 
-        }, INTERVAL)
+            images[oldImage].classList += " hide"
+            
+			setTimeout(function(){
+                images[oldImage].classList = ""
+            }, 2200)
+            
+            currentImage = randomNext(oldImage)
+            images[currentImage].classList += " current"
+			start()
+
+		}, 3000)
     }
 
-    function getRandomNext(){
-        var n = Math.floor(Math.random()*numImages)
-        if(n == current){
-            while(n == current){
-                n = Math.floor(Math.random()*numImages)
+    function randomNext(oldImage) {
+        let newImage = Math.floor(Math.random() * images.length)
+        if(newImage == oldImage) {
+            while(newImage == oldImage) {
+                newImage = Math.floor(Math.random() * images.length)
             }
-        }
-        return n
+        } 
+        return newImage
     }
-
-    // 
-    // $(window).one('enter.page.bq', function(){
-
-        // init_timers[init_timers.length] = setTimeout(function(){
-            // $title.addClass('show');
-        // }, 0);
-
-        // init_timers[init_timers.length] = setTimeout(function(){
-            // $images.eq(current).addClass('current');
-            // start();
-        // }, 800);
-
-        // init_timers[init_timers.length] = setTimeout(function(){
-            // $('#scroll-info').addClass('show');
-        // }, 1300);
-
-        // init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-    // }).one('exit.page.bq', function(){
-        // removeEvents();
-        // stop();
-        // for (var i = init_timers.length - 1; i >= 0; i--) {
-            // clearTimeout(init_timers[i]);
-        // }
-    // })
-
 
 
 })()
-
-// var timer = subTimer = null;
-
-
-// function stop(){
-//     clearTimeout(timer);
-//     clearTimeout(subTimer);
-// }
-
-
-// var init_timers = []
-// $(window).one('enter.page.bq', function(){
-
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $title.addClass('show');
-//     }, 0);
-
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $images.eq(current).addClass('current');
-//         start();
-//     }, 800);
-
-//     init_timers[init_timers.length] = setTimeout(function(){
-//         $('#scroll-info').addClass('show');
-//     }, 1300);
-
-//     init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-// }).one('exit.page.bq', function(){
-//     removeEvents();
-//     stop();
-//     for (var i = init_timers.length - 1; i >= 0; i--) {
-//         clearTimeout(init_timers[i]);
-//     }
-// })
